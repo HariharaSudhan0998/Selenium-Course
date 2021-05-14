d7e69f5274c9a8f2466f0be82b170455c59fed1f
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-14 13:32:39 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,7 +12,16 @@ public class FirstTest {
 
     @Test
     public void OpenBrowser()  {
-        WebDriver driver;
+        public static void main(String[] args) {
+    	
+    	
+    	System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        WebDriver driver =new FirefoxDriver();  
+         driver.get("htttp://flogile.com");
+        // driver.get("htttp://65.2.108.33:8080");
+        //driver.get("http://seleniumhq.org/");	 
+   }
+       /* WebDriver driver;
        // System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
         System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
         WebDriver driver =new FirefoxDriver();
@@ -21,7 +30,7 @@ public class FirstTest {
        // driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google")); */
     }
 
 
