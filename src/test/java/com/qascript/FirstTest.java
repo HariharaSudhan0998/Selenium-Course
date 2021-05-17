c86162c589cd64df701590dffee492b62cbc2d2e
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-17 15:37:57 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -17,11 +17,26 @@ public class FirstTest {
 //        options.addArguments("headless");
          options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-        driver.get("http://flogile.com");
-        //driver.get("https://www.google.com");
-      //  System.out.println(driver.getTitle());
-      //  Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Flogile Technologies"));
+       // driver.get("http://flogile.com");
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
 
+
+}
+
+public class FirstTest {
+
+
+    
+    public static void main(String[] args) {
+    	
+    	
+    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        WebDriver driver =new ChromeDriver();   
+        driver.get("http:flogile.com");
+        //driver.get("http://seleniumhq.org/");	 
+   }
 
 }
