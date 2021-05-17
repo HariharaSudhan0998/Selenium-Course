c362b2fb88f0d6bbc954253c30b7168ff4842d5a
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-17 09:29:14 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -3,37 +3,24 @@ package com.qascript;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.geckodriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FirstTest {
 
 
     @Test
-    //public void OpenBrowser()  {
-        public static void main(String[] args) {
-    	
-    	
-    	System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-        WebDriver driver =new FirefoxDriver();  
-         driver.get("htttp://flogile.com");
-        // driver.get("htttp://65.2.108.33:8080");
-        //driver.get("http://seleniumhq.org/");	 
-   }
-       /* WebDriver driver;
-       // System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-        WebDriver driver =new FirefoxDriver();
-        //ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-       // driver = new ChromeDriver(options);
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google")); */
- //   }
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+    }
 
 
 }
-
-
