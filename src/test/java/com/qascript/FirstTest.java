317a7cc22d5f02c3d8d900f191f058476591df9c
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-18 12:31:35 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,10 +1,9 @@
 package com.qascript;
 
-import org.junit.Assert;
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 
 
